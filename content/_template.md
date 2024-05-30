c96a099b8f34122f5c248da761e8f10c6e3024f0
Aleksandr Zhedik <a.zhedik@gmail.com>
2024-05-30 23:48:04 +0300

    Quartz sync: May 30, 2024, 11:48 PM

--- content/_template.md
+++
@@ -1,5 +0,0 @@
----
-publish: false
----
-# Header
-
--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "jediinprocrastination.github.io",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["private", "templates", ".obsidian", "!(Assets/Public)**/!(*.md)", "!(*.md)"],
     defaultDateType: "created",
     theme: {
       fontOrigin: "googleFonts",
